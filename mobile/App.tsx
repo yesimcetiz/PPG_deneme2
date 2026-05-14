d4e48bc7a969e3e9cee543b96a900c709512ccbf
yesimcetiz <yesimcetiz@gmail.com>
2026-05-14 12:38:57 +0300

    feat: Sentry error tracking entegrasyonu (mobile)

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import * as Sentry from '@sentry/react-native';
 import RootNavigator from './src/navigation/RootNavigator';
 import { useStressNotifications } from './src/hooks/useStressNotifications';
+
+Sentry.init({
+  dsn: 'https://e6417a7723d16c32d3b606aa3e4c1f04@o4511387536654336.ingest.de.sentry.io/4511387569094736',
+  environment: __DEV__ ? 'development' : 'production',
+  tracesSampleRate: 0.2,
+  // Kişisel veri gönderme
+  sendDefaultPii: false,
+});
 
 function AppContent() {
   // Stres yüksekken bildirim gönder
@@ -9,11 +18,11 @@ function AppContent() {
   return <RootNavigator />;
 }
 
-export default function App() {
+export default Sentry.wrap(function App() {
   return (
     <>
       <StatusBar style="light" />
       <AppContent />
     </>
   );
-}
+});
